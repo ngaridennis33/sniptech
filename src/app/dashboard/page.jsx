0e1b7dbfcd93af1993603a0afc1ea148977fe872
ngaridennis33 <ngaridennis33@gmail.com>
2023-06-16 01:17:04 +0300

    Updated the dashboard login page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import styles from './page.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Dashboard = () => {
+
+  
+
   return (
-    <div>Dashboard</div>
+    <div className={styles.wrapper}>
+    <div className={styles.container}>
+      <div className={styles.header}>
+      <Link href="/" className='logoText'>Sniptech</Link>
+        <h1 className='text-heading'>Welcome</h1>
+        <span className={styles.textbody}> Log in to SnipTech to continue to site</span>
+      </div>
+      <form className={styles.form}>
+        <input className={styles.input} autoFocus type="text" placeholder='Email address' required></input>
+        <input className={styles.input} type="password" placeholder='Password' required></input>
+        <span className='text-link'> Forgot Password? </span>
+        <button className={styles.button}>Continue</button>
+      </form>
+
+    <div className={styles.register}>
+      <span> Don&apos;t have an account?</span>
+      <Link className='text-link' href={"dashboard/register"}>Sign Up</Link>
+    </div>
+
+{/* Or Container */}
+    <div className={styles.orCont}>
+      <hr className={styles.hr}/>
+      <span className={styles.orText}>OR</span>
+      <hr className={styles.hr}/>
+
+    </div>
+
+    <div className={styles.socialLogin}>
+      <Image width={20} height={20} src="/5.png" alt="google" />
+      <span>Continue with Google</span>
+    </div>
+    </div>
+    </div>
   )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,19 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from "/public/hero.png"
 
 export default function Home() {
   return (
-  <div className={styles.container}>Home</div>
+  <div>
+    <div className={styles.item}>
+      <h1>Better Design tour digital products</h1>
+     <p> Turning your idea into reality. We bring together the tetam for global tech industry</p>
+     <button> See our works</button>
+    </div>
+
+    <div className={styles.item}>
+    <Image width={500} height={500}  src={Hero} alt='Hero.png' className={styles.img}/>
+    </div>
+  </div>
   )
 }
