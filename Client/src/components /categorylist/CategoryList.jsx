f44b43c20c241a234f1940dc662678b934bc2367
ngaridennis33 <ngaridennis3@gmail.com>
2023-09-14 10:19:57 +0300

    Added the featured component

--- Client/src/components /categorylist/CategoryList.jsx
+++ Client/src/components /categorylist/CategoryList.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import styles from "./categoryList.module.scss"
+import Link from 'next/link'
+import Image from 'next/image'
 
 const CategoryList = () => {
   return (
-    <div>CategoryList</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}> Popular Categories</h1>
+      <div className={styles.categories}>
+          <Link href="/blog?cat=style">
+          <Image src="/contact.png" width={12} height={32} className={styles.img} alt=""/> </Link>
+        </div>
+    </div>
   )
 }
 
--- Client/src/components /featured/Featured.jsx
+++ Client/src/components /featured/Featured.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import styles from "./featured.module.scss"
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, welcome to SnipTech</b> Discover new stories and creative Ideas
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/apps.jpg" alt='' fill className={styles.img}/>
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.PostTitle}> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, necessitatibus?</h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa sed ea provident quia, nulla voluptatem sunt dolores, minima aut corporis id earum molestiae hic eligendi. Quaerat aliquid unde dicta accusamus.
+          </p>
+          <Link href="/readmore" className={styles.authLink} styles={{width:"maxContent"}}>
+          <button className={styles.ctaButton}>Read More</button>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
