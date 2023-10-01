4ab6b0022cc9c459cfcb2d147510c5cd3af44109
ngaridennis33 <ngaridennis33@gmail.com>
2023-10-01 19:44:51 +0300

    Updated the category section

--- Client/src/components /categorylist/CategoryList.jsx
+++ Client/src/components /categorylist/CategoryList.jsx
@@ -8,7 +8,7 @@ const CategoryList = () => {
     <div className={styles.container}>
       <h1 className={styles.title}> Popular Categories</h1>
       <div className={styles.categories}>
-          <Link href="/blog?cat=style" className = {`${styles.category} ${styles.style}`}>
+          {/* <Link href="/blog?cat=style" className = {`${styles.category} ${styles.style}`}>
           <Image src="/contact.png" width={32} height={32} className={styles.img} alt=""/> 
           style
           </Link>
@@ -26,7 +26,62 @@ const CategoryList = () => {
           <Link href="/blog?cat=tech" className = {`${styles.category} ${styles.tech}`}>
           <Image src="/bg2.jpg" width={32} height={32} className={styles.img} alt=""/> 
           Tech
-          </Link>
+          </Link> */}
+
+          <div className={styles.card}>
+          <div className={styles.left}>
+          <Image src="/bag.svg" width={32} height={32} className={styles.img} alt=""/> 
+            <h2 className={styles.subTitle}>Travel</h2>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className={styles.right}>
+          <Image src="/car.svg" width={32} height={32} className={styles.img} alt=""/> 
+            </div>
+          </div>
+
+          <div className={styles.card}>
+          <div className={styles.left}>
+          <Image src="/bag.svg" width={32} height={32} className={styles.img} alt=""/> 
+            <h2 className={styles.subTitle}>Travel</h2>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className={styles.right}>
+          <Image src="/car.svg" width={32} height={32} className={styles.img} alt=""/> 
+            </div>
+          </div>
+
+          <div className={styles.card}>
+          <div className={styles.left}>
+          <Image src="/bag.svg" width={32} height={32} className={styles.img} alt=""/> 
+            <h2 className={styles.subTitle}>Travel</h2>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className={styles.right}>
+          <Image src="/car.svg" width={32} height={32} className={styles.img} alt=""/> 
+            </div>
+          </div>
+
+          <div className={styles.card}>
+          <div className={styles.left}>
+          <Image src="/bag.svg" width={32} height={32} className={styles.img} alt=""/> 
+            <h2 className={styles.subTitle}>Travel</h2>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className={styles.right}>
+          <Image src="/car.svg" width={32} height={32} className={styles.img} alt=""/> 
+            </div>
+          </div>
+
+          <div className={styles.card}>
+          <div className={styles.left}>
+          <Image src="/bag.svg" width={32} height={32} className={styles.img} alt=""/> 
+            <h2 className={styles.subTitle}>Travel</h2>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className={styles.right}>
+          <Image src="/car.svg" width={32} height={32} className={styles.img} alt=""/> 
+            </div>
+          </div>
         </div>
     </div>
   )
