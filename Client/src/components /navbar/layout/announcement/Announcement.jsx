3234064922219c153f07f2c7774cd62be06c18cf
ngaridennis33 <ngaridennis33@gmail.com>
2023-10-04 00:28:51 +0300

    Added the search icons

--- Client/src/components /navbar/layout/announcement/Announcement.jsx
+++ Client/src/components /navbar/layout/announcement/Announcement.jsx
@@ -4,7 +4,11 @@ import Image from 'next/image'
 
 const Announcement = () => {
   return (
-    <div className={styles.container} id={styles.flex}>
+    <div className={styles.container}>
+      <div className={styles.searchBox}>
+        <input className={styles.input} type="text" placeholder="Type here..." />
+        <Image className={styles.icon} src="plus.svg" alt="" width={15} height={15}/>
+      </div>
         <div className={styles.social}>
           <Image src="/1.png" width={20} height={20} className={styles.icon} alt="sniptech fb"/>
           <Image src="/2.png" width={20} height={20} className={styles.icon} alt="sniptech"/>
