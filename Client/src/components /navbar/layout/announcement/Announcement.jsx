bce6efda57cd7f47fc1792736ed81a50545f3140
ngaridennis33 <ngaridennis33@gmail.com>
2023-10-04 12:07:55 +0300

    Added the search icon

--- Client/src/components /navbar/layout/announcement/Announcement.jsx
+++ Client/src/components /navbar/layout/announcement/Announcement.jsx
@@ -7,7 +7,7 @@ const Announcement = () => {
     <div className={styles.container}>
       <div className={styles.searchBox}>
         <input className={styles.input} type="text" placeholder="Type here..." />
-        <Image className={styles.icon} src="plus.svg" alt="" width={15} height={15}/>
+        <Image className={styles.icon} src="search.svg" alt="" width={15} height={15}/>
       </div>
         <div className={styles.social}>
           <Image src="/1.png" width={20} height={20} className={styles.icon} alt="sniptech fb"/>
