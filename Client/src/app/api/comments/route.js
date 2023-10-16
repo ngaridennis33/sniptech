aafc0e4e614785ed1101829c02c32b0c862470ec
ngaridennis33 <ngaridennis33@gmail.com>
2023-10-16 16:15:19 +0300

    Updated the coment section

--- Client/src/app/api/comments/route.js
+++ Client/src/app/api/comments/route.js
@@ -26,20 +26,20 @@ export const GET = async (req) => {
 
 // CREATE A NEW COMMENTS OF A POST
 export const POST = async (req) => {
-  const session = getAuthSession();
-
-  const { searchParams } = new URL(req.url);
-  const postSlug = searchParams.get("postSlug");
+  const session = await getAuthSession();
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Not authenticated!" }, { status: 401 })
+    );
+  }
 
   try {
-    const comments = await prisma.comment.findMany({
-      where: {
-        ...(postSlug && { postSlug }),
-      },
-      include: { user: true },
+    const body = await req.json();
+    const comment = await prisma.comment.create({
+      data: { ...body, userEmail: session.user.email },
     });
 
-    return new NextResponse(JSON.stringify(comments, { status: 200 }));
+    return new NextResponse(JSON.stringify(comment, { status: 200 }));
   } catch (err) {
     console.log(err);
     return new NextResponse(
--- Client/src/components /comments/Comments.jsx
+++ Client/src/components /comments/Comments.jsx
@@ -1,10 +1,11 @@
 "use client" 
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./comments.module.scss"
 import Link from 'next/link';
 import Image from 'next/image';
 import useSWR from "swr"
 import { useSession } from 'next-auth/react';
+import Spinner from '../spinner/spinner';
 
 const fetcher = async (url) => {
   const res = await fetch(url);
@@ -20,21 +21,31 @@ const fetcher = async (url) => {
 
 const Comments = ({postSlug}) => {
 
+  
   const {status} = useSession();
-  const {data, isLoading} = useSWR(`http://localhost:3000/api/comments?postSlug=${postSlug}`,fetcher)
+  const {data,mutate, isLoading} = useSWR(`http://localhost:3000/api/comments?postSlug=${postSlug}`,fetcher)
+  
+  const [desc,setDesc] = useState("");
 
+  const handleSubmit = async () => {
+    await fetch("/api/comments",{
+      method:"POST",
+      body:JSON.stringify({desc,postSlug})
+    });
+    mutate();
+  }
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}> Comments</h1>
       {status === "authenticated" ? (<div className={styles.write}>
-        <textarea className={styles.input} placeholder='write a comment..'/>
-        <button className={styles.action}>Send</button>
+        <textarea className={styles.input} placeholder='write a comment..' onChange={(e)=> setDesc(e.target.value)} />
+        <button className={styles.action} onClick={handleSubmit}> Send </button>
       </div>) : (<Link href="/login"> Login in to write a comment</Link>)}
      <div className={styles.comments} >
 
-   {isLoading ? "loading": data?.map((item)=>(
-      <div className={styles.comment} key={item.__id}>
+   {isLoading ? <Spinner/>: data?.map((item)=>(
+      <div className={styles.comment} key={item._id}>
         <div className={styles.user}>
           {item?.user?.image && <Image src={item.user.image} alt ="" width={50} height={50} className={styles.image} />}
           <div className={styles.userInfo}>
